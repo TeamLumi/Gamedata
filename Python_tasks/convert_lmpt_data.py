efd24fba58af5179047255deb01d4fae94c7428f
AarCon <bowtiesarecool110@gmail.com>
2023-06-06 07:39:17 -0600

    Test fixed

--- Python_tasks/convert_lmpt_data.py
+++ Python_tasks/convert_lmpt_data.py
@@ -207,24 +207,28 @@ def check_mons_list(pokemon_list, zoneID):
     for mon in pokemon_list:
         if mon[1] == 'ground_mons' or mon[1] == 'day' or mon[1] == 'night':
             original_list.append(mon[0])
-
     active_list = copy.deepcopy(original_list)
     radar_count = 0
     active_list[9] = active_list[1]
     active_list[10] = active_list[4]
     active_list[11] = active_list[5]
+    radar_list = {zoneID: []}
     for mon in pokemon_list:
-        if mon[1] == 'swayGrass' and mon[2] == 3:
-            active_list[1] = mon
+        if mon[1] == 'swayGrass':
+            radar_list[zoneID].append(get_pokemon_name(mon[0]))
+            active_list[1] = mon[0]
         if mon[1].startswith('gba') and mon[2] != 2 and radar_count < 2:
             radar_count += 1
             if radar_count == 1:
-                active_list[4] = mon
+                active_list[4] = mon[0]
             if radar_count == 2:
-                active_list[5] = mon
+                active_list[5] = mon[0]
     for mon in original_list:
         if mon not in active_list:
             missing_list.append(get_pokemon_name(mon))
+    unique_radar_list = list(set(radar_list[zoneID]))
+    if len(unique_radar_list) > 1:
+        print(radar_list)
     unique_list = list(set(missing_list))
     if len(unique_list) > 0 and zoneID not in final_list.keys():
         final_list[pokemon_list[0][3]] = unique_list
