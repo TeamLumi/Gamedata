b6dcff46bb38b97668cd7efe77ec35543ff67fea
AarCon <bowtiesarecool110@gmail.com>
2023-06-15 22:40:10 -0600

    Add more comments

--- Python_tasks/data_checks.py
+++ Python_tasks/data_checks.py
@@ -8,21 +8,31 @@ from pokemonUtils import (get_form_name, get_form_pokemon_personal_id,
                           get_pokemon_mons_no_from_name)
 
 def check_monsName(monsName):
+    '''
+    This checks that a pokemons name is formatted correctly specifically for the Honey trees
+    '''
     monsNo = get_pokemon_mons_no_from_name(monsName)
     if monsNo == -1:
         print(monsName)
     pass
 
 def get_average_time(execution_times):
+    # Do I really need to explain this one?
     return sum(execution_times) / len(execution_times)
 
 def bad_encounter_data(pkmn_name, routeName, route):
+    '''
+    This is useful for checking whether there are bad encounters within the diff_forms
+    '''
     bad_encounters = []
     print('BAD ENCOUNTER', pkmn_name, routeName, route)
     bad_encounters.append({pkmn_name, routeName, route})
     return bad_encounters
 
 def check_bad_encounter(encounters, tracker_route, pkmn_key, lumi_formula_mon, temp_form_no, zoneID, method=''):
+    '''
+    This checks the methods that are on each route and verifies that no pokemon is missing
+    '''
     name_routes = full_data['name_routes']
 
     bad_encounters = []
@@ -41,6 +51,22 @@ def check_bad_encounter(encounters, tracker_route, pkmn_key, lumi_formula_mon, t
         return -2
 
 def check_mons_list(pokemon_list, zoneID, final_list):
+    '''
+    This checks the methods that are on each route and verifies that no pokemon is missing.
+    Here's what's happening because it's mighty confusing:
+        Generated a list of the ground_mons
+        Made a copy of that list to make changes to it.
+        In the copy of the list, I did the following:
+            moved the 2nd slot mon to the 10th slot (and deleted the pokemon that was in slot 10 before, same thing with the rest)
+            moved the 5th slot mon to the 11th slot
+            moved the 6th slot mon to the 12th slot
+        For the radar and incense mons I did the following to the copy of the list:
+            Changed the 2nd slot of the list to be the 4th slot of the radar
+            Changed the 5th slot to be the 1st slot of the very first incense category ('gbaRuby' unless there's something out of order).
+            Changed the 6th slot to be the 2nd slot of the incense.
+        Compared the contents of the two lists (original_list and copy_of_list) to find if there were pokemon that were in the original_list and not in the copy_of_list
+        If there were pokemon that were in original_list and not in the copy_of_list, a flag goes up and throws a warning.
+    '''
     original_list = []
     missing_list = []
     incense_count = 0
