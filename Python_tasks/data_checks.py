999ed64d7bbf8df8e8307f830c6c9bd05a0294f0
AarCon <bowtiesarecool110@gmail.com>
2023-08-03 19:33:28 -0600

    Fix Incense Bug

--- Python_tasks/data_checks.py
+++ Python_tasks/data_checks.py
@@ -71,15 +71,20 @@ def check_mons_list(pokemon_list, zoneID, final_list):
         Generated a list of the ground_mons
         Made a copy of that list to make changes to it.
         In the copy of the list, I did the following:
-            moved the 2nd slot mon to the 10th slot (and deleted the pokemon that was in slot 10 before, same thing with the rest)
+            moved the 2nd slot mon to the 10th slot 
+              (and deleted the pokemon that was in slot 10 before, same thing with the rest)
             moved the 5th slot mon to the 11th slot
             moved the 6th slot mon to the 12th slot
         For the radar and incense mons I did the following to the copy of the list:
             Changed the 2nd slot of the list to be the 4th slot of the radar
-            Changed the 5th slot to be the 1st slot of the very first incense category ('gbaRuby' unless there's something out of order).
+            Changed the 5th slot to be the 1st slot of the very first incense category 
+              ('gbaRuby' unless there's something out of order).
             Changed the 6th slot to be the 2nd slot of the incense.
-        Compared the contents of the two lists (original_list and copy_of_list) to find if there were pokemon that were in the original_list and not in the copy_of_list
-        If there were pokemon that were in original_list and not in the copy_of_list, a flag goes up and throws a warning.
+        Compared the contents of the two lists (original_list and copy_of_list) 
+          to find if there were pokemon that were in the original_list 
+          and not in the copy_of_list
+        If there were pokemon that were in original_list and 
+          not in the copy_of_list, a flag goes up and throws a warning.
     '''
     original_list = []
     missing_list = []
