b47b9a83ff89e740fb1ac32dfd6e655b5119e955
AarCon <bowtiesarecool110@gmail.com>
2024-07-07 11:53:29 -0600

    Update ItemTable to include new tms and also swap tms to be more accurate

--- Python_tasks/tm_conversion.py
+++ Python_tasks/tm_conversion.py
@@ -6,6 +6,8 @@ from moveUtils import (
   get_tech_machine_learnset,
   create_tm_learnset,
   convert_int_to_bit,
+  convert_list_to_binary_array,
+  create_move_id_learnset,
 )
 from pokemonUtils import get_mons_no_and_form_no, get_pokemon_name
 from load_files import load_data
@@ -14,6 +16,7 @@ parent_file_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 input_file_path = os.path.join(parent_file_path, constants.INPUT_NAME)
 debug_file_path = os.path.join(parent_file_path, "Python_tasks", constants.DEBUG_NAME)
 stats_file_path = os.path.join(parent_file_path, "Python_tasks", "pokemon_stats")
+
 
 def create_converted_tm_list():
   for pokemon in personal_data["Personal"]:
@@ -57,11 +60,58 @@ def create_converted_tm_list():
     with open( current_filepath, 'w') as file:
       json.dump(outputObj, file, ensure_ascii=False, indent = 2)
 
+def swap_tms_to_move_ids_in_learnsets():
+  for file in os.listdir(os.path.join(debug_file_path, "tm_conversion")):
+    file_path = os.path.join(debug_file_path, "tm_conversion", file)
+    with open(file_path, "r") as input:
+      pokemon_learnset = json.load(input)
+
+    tm_binary_list = convert_list_to_binary_array([
+      pokemon_learnset["set01"],
+      pokemon_learnset["set02"],
+      pokemon_learnset["set03"],
+      pokemon_learnset["set04"],
+    ])
+    tm_compatibility = create_move_id_learnset(tm_binary_list)
+
+    tm_move_id_file_path = os.path.join(debug_file_path, "tm_move_id_learnsets", file)
+    with open(tm_move_id_file_path, "w") as debug_output:
+      json.dump(tm_compatibility, debug_output, ensure_ascii=False, indent=2)
+
+def swap_move_ids_back_to_new_binary_learnsets():
+  for file in os.listdir(os.path.join(debug_file_path, "tm_move_id_learnsets")):
+    file_path = os.path.join(debug_file_path, "tm_move_id_learnsets", file)
+    with open(file_path, "r") as input:
+      pokemon_learnset = json.load(input)
+
+    convertedInts = create_tm_learnset(pokemon_learnset)
+
+    outputObj = {
+      "set01" : 0,
+      "set02" : 0,
+      "set03" : 0,
+      "set04" : 0,
+      "set05" : 0,
+      "set06" : 0,
+      "set07" : 0,
+      "set08" : 0,
+    }
+
+    outputObj["set01"] = convertedInts[0]
+    outputObj["set02"] = convertedInts[1]
+    outputObj["set03"] = convertedInts[2]
+    outputObj["set04"] = convertedInts[3]
+
+    tm_binary_file_path = os.path.join(debug_file_path, "tm_conversion", file)
+    with open(tm_binary_file_path, "w") as debug_output:
+      json.dump(outputObj, debug_output, ensure_ascii=False, indent=2)
+
 if __name__ == "__main__":
   # full_data = load_data()
   # personal_table = full_data["personal_table"]
+  swap_move_ids_back_to_new_binary_learnsets()
 
   # This is only useful to grab from the poke_api_access output files.
-  with open(os.path.join(debug_file_path, "New_Personal_Table.json"), "r") as json_file:
-    personal_data = json.load(json_file)
-  create_converted_tm_list()
+  # with open(os.path.join(debug_file_path, "New_Personal_Table.json"), "r") as json_file:
+  #   personal_data = json.load(json_file)
+  # create_converted_tm_list()
