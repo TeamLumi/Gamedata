b809bb9c631d54af6568d988de45b4a98082af69
AarCon <bowtiesarecool110@gmail.com>
2025-01-17 10:50:51 -0700

    Stashing changes before pulling

--- entrypoint.sh
+++ entrypoint.sh
@@ -24,23 +24,11 @@ git config --global user.email "$INPUT_USER_EMAIL"
 git config --global user.name "$INPUT_USER_NAME"
 git clone --single-branch --branch $INPUT_DESTINATION_BRANCH "https://x-access-token:$API_TOKEN_GITHUB@$INPUT_GIT_SERVER/$INPUT_DESTINATION_REPO.git" "$CLONE_DIR"
 
-if [ ! -z "$INPUT_RENAME" ]; then
-  echo "Setting new filename: ${INPUT_RENAME}"
-  DEST_COPY="$CLONE_DIR/$INPUT_DESTINATION_FOLDER/$INPUT_RENAME"
-else
-  DEST_COPY="$CLONE_DIR/$INPUT_DESTINATION_FOLDER"
-fi
-
-echo "Copying contents to git repo"
-mkdir -p $CLONE_DIR/$INPUT_DESTINATION_FOLDER
-if [ -z "$INPUT_USE_RSYNC" ]; then
-  cp -R "$INPUT_SOURCE_FILE" "$DEST_COPY"
-else
-  echo "rsync mode detected"
-  rsync -avrh "$INPUT_SOURCE_FILE" "$DEST_COPY"
-fi
-
 cd "$CLONE_DIR"
+
+# Stash any pre-staged changes before pulling or switching branches
+echo "Stashing pre-existing changes"
+git stash --include-untracked
 
 if [ ! -z "$INPUT_DESTINATION_BRANCH_CREATE" ]; then
   echo "Creating new branch: ${INPUT_DESTINATION_BRANCH_CREATE}"
@@ -49,6 +37,26 @@ if [ ! -z "$INPUT_DESTINATION_BRANCH_CREATE" ]; then
 else
   echo "Ensuring branch is up-to-date"
   git pull origin "$INPUT_DESTINATION_BRANCH" --rebase
+fi
+
+# Reapply stashed changes
+echo "Reapplying stashed changes"
+git stash pop || echo "No stashed changes to apply"
+
+if [ ! -z "$INPUT_RENAME" ]; then
+  echo "Setting new filename: ${INPUT_RENAME}"
+  DEST_COPY="$CLONE_DIR/$INPUT_DESTINATION_FOLDER/$INPUT_RENAME"
+else
+  DEST_COPY="$CLONE_DIR/$INPUT_DESTINATION_FOLDER"
+fi
+
+echo "Copying contents to git repo"
+mkdir -p "$CLONE_DIR/$INPUT_DESTINATION_FOLDER"
+if [ -z "$INPUT_USE_RSYNC" ]; then
+  cp -R "$INPUT_SOURCE_FILE" "$DEST_COPY"
+else
+  echo "rsync mode detected"
+  rsync -avrh "$INPUT_SOURCE_FILE" "$DEST_COPY"
 fi
 
 if [ -z "$INPUT_COMMIT_MESSAGE" ]; then
